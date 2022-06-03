c450d7af62f62d948113620d76977157f87e1b96
paytondev <81662879+paytontech@users.noreply.github.com>
2022-06-03 16:19:44 -0400

    dark mode???

---
+++ settings.js
@@ -0,0 +1,25 @@
+var darkSettings = localStorage["darkSetting"] || "light"
+function checkDarkMode() {
+    console.log(darkSettings)
+    if (darkSettings == "light") {
+        document.body.style = "background: linear-gradient(151deg, rgba(255,255,255,1) 0%, rgba(209,209,222,1) 35%, rgba(158,181,186,1) 100%); color: black;"
+        document.getElementById("darkmode").innerHTML = `<img src="/assets/darkbutton.png"/>`
+    } else {
+        document.body.style = "background: linear-gradient(151deg, rgba(0,0,0,1) 0%, rgba(38,38,52,1) 35%, rgba(98,102,103,1) 100%); color: white;"
+        document.getElementById("darkmode").innerHTML = `<img src="/assets/lightbutton.png"/>`
+    }
+}
+function toggleDarkMode() {
+    console.log("toggle")
+    if (localStorage["darkSetting"] == "light") {
+        //dark mode
+        localStorage["darkSetting"] = "dark"
+        document.body.style = "background: linear-gradient(151deg, rgba(0,0,0,1) 0%, rgba(38,38,52,1) 35%, rgba(98,102,103,1) 100%); color: white;"
+        document.getElementById("darkmode").innerHTML = `<img src="/assets/lightbutton.png"/>`
+    } else if (localStorage["darkSetting"] == "dark") {
+        //light mode
+        localStorage["darkSetting"] = "light"
+        document.body.style = "background: linear-gradient(151deg, rgba(255,255,255,1) 0%, rgba(209,209,222,1) 35%, rgba(158,181,186,1) 100%); color: black;"
+        document.getElementById("darkmode").innerHTML = `<img src="/assets/darkbutton.png"/>`
+    }
+}
